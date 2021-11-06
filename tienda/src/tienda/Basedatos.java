fdafece85f23ae1e49b09eb770512583344add7e
Albres20 <david_albres20@hotmail.com>
2021-11-05 20:40:33 -0500

    Base de datos coneccion

---
+++ tienda/src/tienda/Basedatos.java
@@ -0,0 +1,32 @@
+
+package tienda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Basedatos {
+    static final String ROOT= "root";
+    static final String URL = "jdbc:mysql://localhost:3306/bd?useSSL=false";
+    
+    Connection con;
+    Statement st;
+    ResultSet rs;
+    
+    public Basedatos (){
+        //constructor vacio
+    }
+
+    public Connection conectar(){
+        try{
+            con = DriverManager.getConnection(URL,ROOT,"1234");
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No se pudo conectar a la base de datos");
+        }
+        return con;
+    }
+}
