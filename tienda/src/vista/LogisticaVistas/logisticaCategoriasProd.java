ade3f0bff8457e2e48544e974a363ab4aa2b2a0b
Albres20 <david_albres20@hotmail.com>
2022-04-15 19:40:39 -0500

    agregue bd

--- tienda/src/vista/LogisticaVistas/logisticaCategoriasProd.java
+++ tienda/src/vista/LogisticaVistas/logisticaCategoriasProd.java
@@ -137,27 +137,21 @@ public class logisticaCategoriasProd extends javax.swing.JFrame {
             }
         });
 
-        botonEliminarModProducto.setBackground(new java.awt.Color(0, 0, 0));
-        botonEliminarModProducto.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        botonEliminarModProducto.setForeground(new java.awt.Color(255, 255, 255));
-        botonEliminarModProducto.setText("Eliminar");
-
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(266, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 257, Short.MAX_VALUE))
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(botonGuardarLogisiticaCategorias)
-                        .addGap(74, 74, 74)
-                        .addComponent(botonEliminarModProducto))
-                    .addComponent(jLabel10, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addComponent(jLabel10)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(botonGuardarLogisiticaCategorias)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
@@ -167,11 +161,9 @@ public class logisticaCategoriasProd extends javax.swing.JFrame {
                 .addComponent(jLabel10)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(botonGuardarLogisiticaCategorias)
-                    .addComponent(botonEliminarModProducto))
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(botonGuardarLogisiticaCategorias)
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         jPanel3.setBackground(new java.awt.Color(72, 104, 218));
@@ -508,7 +500,6 @@ public class logisticaCategoriasProd extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public static final javax.swing.JButton botonEliminarModProducto = new javax.swing.JButton();
     public static final javax.swing.JButton botonGuardarLogisiticaCategorias = new javax.swing.JButton();
     public static final javax.swing.JButton botonSalirLogisticaModifProd = new javax.swing.JButton();
     private static final javax.swing.JButton categoria = new javax.swing.JButton();
